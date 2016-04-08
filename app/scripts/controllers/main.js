e8371f168f1d2b4beea947d82a8a7fbad1189fa8
Eoin O'Brien <eoin@eoinobrien.ie>
2016-04-08 01:02:58 +0100

    Inital design

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the App
+ */
+angular.module('school-house')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
